62b6cf267fae06ad27b2799d7dd5783bd32d7853
Luke Smetham <luke@arkitek.co.uk>
2020-03-16 15:52:26 +0100

    ore partner attachment styling

--- src/Message/MessageAttachment.js
+++ src/Message/MessageAttachment.js
@@ -7,11 +7,11 @@ const Root = styled.div`
     border: 1px solid ${({ theme }) => theme.color.border};
     border-top-left-radius: ${({ isOwn, theme }) =>
         !isOwn ? theme.borderRadius : theme.borderRadius * 2}px;
-    border-top-right-radius: ${({ hasPrev, isOwn, theme }) =>
-        hasPrev && !isOwn ? theme.borderRadius : theme.borderRadius * 2}px;
+    border-top-right-radius: ${({ isOwn, theme }) =>
+        !isOwn ? theme.borderRadius : theme.borderRadius * 2}px;
     border-bottom-left-radius: ${({ isOwn, theme }) =>
         !isOwn ? theme.borderRadius : theme.borderRadius * 2}px;
-    border-bottom-right-radius: ${({ isOwn, hasNext, theme }) =>
+    border-bottom-right-radius: ${({ isOwn, theme }) =>
         !isOwn ? theme.borderRadius * 2 : theme.borderRadius}px;
     max-width: 400px;
     overflow: hidden;
